87fa061f1cffc454b95ef8a52442c020a564a9eb
lorenzo <lorenzo.brito@gmail.com>
2020-03-15 17:43:12 -0600

    doing great

--- webapp/src/listuser.js
+++ webapp/src/listuser.js
@@ -5,7 +5,16 @@ import axios from 'axios';
 function ListUser() {
   const apiUrl = "https://apifacelorbrito.azurewebsites.net/api/faceapi?code=LSPc9n6WBiC33f2u9GFlvrygmfLEVcw4OJgeDVaYMPJIBig6yKv6yA==";
   const [List, setList] = useState([]);
-    
+  const [Files, setFiles] = useState([]);
+  const [Container, setContainer] = useState('');
+  function getFiles (item) {
+    axios.get(apiUrl+'&container='+item)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        setFiles(res.data)
+      })
+  }
       useEffect(() => {
         console.log('calling use');
         const fetchData = async () => {
@@ -19,10 +28,21 @@ function ListUser() {
      
       return (
         <div>
-      
+      <div>
        {List.map((item,index)=>{
-              return  <div key={index}>{item}</div>
+              return  <div key={index}>
+              <button onClick={() => getFiles(item)}>{item}</button>
+              </div>
        })}
+       </div>
+       <div>
+       {Files.map((item,index)=>{
+              return  <div key={index}>
+              <button >{item}</button>
+              <img src={item} ></img>
+              </div>
+       })}
+       </div>
         </div>
       );
 }
